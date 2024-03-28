018aae826aa2b4b7eadeac55b1387bd4e8e92b7b
Jon0945 <john.gullne@gmail.com>
2024-03-28 10:04:39 +0100

    ToDoItem classes updated. getSummary removed. Overrides for equals, hashcode and toString added.

--- src/main/java/se/lexicon/models/TodoItemTask.java
+++ src/main/java/se/lexicon/models/TodoItemTask.java
@@ -1,5 +1,7 @@
 package se.lexicon.models;
 
+import java.util.Objects;
+
 public class TodoItemTask {
     //Static field
     private static int taskCounter = 1;
@@ -40,10 +42,24 @@ public class TodoItemTask {
         }
         return "No";
     }
-    public String getSummary() {
-        return "\nID: " + id +
-                "\nAssigned: " + this.isTrue(assigned) +
-                "\nTask: " + this.todoItem.getTitle() +
-                "\nTask Assigned To: " + this.assignee.getFirstName() + " " + this.assignee.getLastName();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TodoItemTask that = (TodoItemTask) o;
+        return id == that.id && assigned == that.assigned && Objects.equals(todoItem, that.todoItem);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, assigned, todoItem);
+    }
+
+    @Override
+    public String toString() {
+        return "TodoItemTask{" +
+                "id=" + id +
+                ", assigned=" + assigned +
+                ", todoItem=" + todoItem +
+                '}';
     }
 }
